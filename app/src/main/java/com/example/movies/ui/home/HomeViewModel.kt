7711429c2280257bdc8f1cd437ce886b80298747
Artem Polovin <polovinartem@gmail.com>
2021-06-24 18:18:19 -0700

    [task-5] fixed updating movie list when press back arrow from movieDetails screen

--- app/src/main/java/com/example/movies/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/movies/ui/home/HomeViewModel.kt
@@ -7,7 +7,6 @@ import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import com.example.data.datasource.MoviePagingSource
-import com.example.data.repositories_impl.MoviesRepositoryImpl
 import com.example.domain.models.PopularMovieWithDetailsModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
@@ -18,14 +17,23 @@ class HomeViewModel @Inject constructor(
     private val moviePagingSource: MoviePagingSource
 ) : ViewModel() {
 
-     fun fetchPopularMoviesWithDetails(): Flow<PagingData<PopularMovieWithDetailsModel>>{
+    private var lastFetchedMovieResult: Flow<PagingData<PopularMovieWithDetailsModel>>? = null
 
-             return Pager(config = PagingConfig(
-                 pageSize = 20,
-                 enablePlaceholders = false
-             ),
-                 pagingSourceFactory = { moviePagingSource }
-             ).flow
-         }
+    fun fetchPopularMoviesWithDetails(): Flow<PagingData<PopularMovieWithDetailsModel>> {
+
+        val lastMovieResult = lastFetchedMovieResult
+        if (lastMovieResult != null) return lastMovieResult
+
+        val newFetchedMovieResult = Pager(config = PagingConfig(
+            pageSize = 20,
+            enablePlaceholders = false
+        ),
+            pagingSourceFactory = { moviePagingSource }
+        ).flow.cachedIn(viewModelScope)
+
+        lastFetchedMovieResult = newFetchedMovieResult
+
+        return newFetchedMovieResult
+    }
 
 }
