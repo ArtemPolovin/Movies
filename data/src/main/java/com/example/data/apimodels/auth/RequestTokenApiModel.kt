9f4a1610081e40a033a686efe10147675a5cff2a
Artem_Polovin <artem@cazton.com>
2022-06-13 22:43:28 -0700

    [MOV-10]Added app optimization and shrinking

--- data/src/main/java/com/example/data/apimodels/auth/RequestTokenApiModel.kt
+++ data/src/main/java/com/example/data/apimodels/auth/RequestTokenApiModel.kt
@@ -1,6 +1,6 @@
 package com.example.data.apimodels.auth
 
-data class RequestTokenApiModel(
+ data class RequestTokenApiModel(
     val expires_at: String,
     val request_token: String,
     val success: Boolean
