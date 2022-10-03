2ae51490daa219e89952b6296f8b91c8eb336763
Artem_Polovin <artem@cazton.com>
2022-10-02 19:01:04 -0700

    [TASK-MOV-12] created offline mode for screens which use paging lib

--- domain/src/main/java/com/sacramento/domain/usecases/movie_usecase/GetAllSavedMoviesFromDbUseCase.kt
+++
@@ -1,7 +0,0 @@
-package com.sacramento.domain.usecases.movie_usecase
-
-import com.sacramento.domain.repositories.MoviesRepository
-
-class GetAllSavedMoviesFromDbUseCase(private val moviesRepository: MoviesRepository) {
-    suspend operator fun invoke() =  moviesRepository.getMovieListFromDb()
-}
